6e4f63bb972e196e2c7849ac9536de48a4a79992
JoaoPw <dev_joaopedro@outlook.com>
2019-11-15 19:09:49 -0300

    Aplicando o props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
     <div className="App">
       <h1>Hello React</h1>
       <p>This is realy working</p>
-      <Person />
+      <Person name="JP" age="21"/>
+      <Person name="HB" age="87"/>
+      <Person name="EB" age="85"/>
     </div>
   );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello React'))
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-const person = () => {
-return <p>Im' a person who has {Math.floor(Math.random()* 30)} years old</p>
+const person = (props) => {
+return <p>I am {props.name} and I am {props.age} years old</p>
 } 
 
 export default person 
